96477d8e7b93d4fcbaa33d1291d507ac4dac5d7c
wysockey <twysockey@sjnma.org>
2018-02-05 10:20:15 -0600

    learned string variables>

---
+++ variables.py
@@ -0,0 +1,6 @@
+
+print("hello world!")
+first_name = raw_input("what is your first  name?")
+last_name = raw_input(" what is your last name?") 
+full_name = first_name +" "+ last_name
+print(full_name)
